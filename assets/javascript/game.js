7a7565f5091a1d7cae49abbb6242e15792fbc5c9
Ball Man <joshua.christensen13@gmail.com>
2021-01-20 22:36:45 -0700

    figured out code to make the images different crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,38 +2,41 @@ var wins = 0;
 var losses = 0;
 randomNumber;
 totalScore = 0;
+var pics = ["/assets/images/crystal1.jpg", "/assets/images/crystal2.jpg", "/assets/images/gem1.jpg", "/assets/images/gem2.jpg"]
 
-function begin(){
-    
-$(".crystals").empty();
+$(document).ready(function(){
 
-randomNumber = Math.floor(Math.random() * 101) + 19;
+function begin() {
 
-$("#randomNumber").text(randomNumber);
+    $(".crystals").empty();
 
-console.log(randomNumber);
+    randomNumber = Math.floor(Math.random() * 101) + 19;
+
+    $("#randomNumber").text(randomNumber);
+
+    console.log(randomNumber);
 
 
-    for (var i = 0; i < 4; i++){
-        var randomValue = Math.floor(Math.random() * 11) +1;
-        console.log(randomValue);       
+    for (var i = 0; i < 4; i++) {
+        var randomValue = Math.floor(Math.random() * 11) + 1;
+        console.log(randomValue);
 
-        var imageCrystal = $("<img>");
+        var imageCrystal = $("<img src = '" + pics[i] + "'>");
 
         imageCrystal.addClass("crystal-image");
 
-        imageCrystal.attr("src", "assets/images/gem2.jpg");
+        // imageCrystal.attr("src", pics[i]);
 
         imageCrystal.attr("data-random-value", randomValue);
 
         $(".crystals").append(imageCrystal);
 
     };
-    
+
 };
 begin();
 
-$(document).on("click", ".crystal-image", function() {
+$(document).on("click", ".crystal-image", function () {
 
     var num = parseInt($(this).attr("data-random-value"));
 
@@ -42,19 +45,23 @@ $(document).on("click", ".crystal-image", function() {
     $("#totalScore").text(totalScore);
     console.log(totalScore);
 
-    if(totalScore > randomNumber){
+    if (totalScore > randomNumber) {
         losses++;
         $("#losses").text(losses);
         begin();
         totalScore = 0;
+        alert("You Lose");
         console.log("you lose")
     }
 
-    else if(totalScore === randomNumber){
+    else if (totalScore === randomNumber) {
         wins++;
         $("#wins").text(wins);
         begin();
         totalScore = 0;
+        alert("You Win");
     }
 
-  });
+});
+
+});
