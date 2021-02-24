c255e5aa340414f800143506e5e893cdc28a1eda
Ball Man <joshua.christensen13@gmail.com>
2021-02-23 19:33:17 -0700

    figured out how to make the total score clear after winning or losing when starting a new round.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,64 +4,66 @@ randomNumber;
 totalScore = 0;
 var pics = ["/assets/images/crystal1.jpg", "/assets/images/crystal2.jpg", "/assets/images/gem1.jpg", "/assets/images/gem2.jpg"]
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-function begin() {
+    function begin() {
 
-    $(".crystals").empty();
+        $(".crystals").empty();
 
-    randomNumber = Math.floor(Math.random() * 101) + 19;
+        randomNumber = Math.floor(Math.random() * 101) + 19;
 
-    $("#randomNumber").text(randomNumber);
+        $("#randomNumber").text(randomNumber);
 
-    console.log(randomNumber);
+        console.log(randomNumber);
 
 
-    for (var i = 0; i < 4; i++) {
-        var randomValue = Math.floor(Math.random() * 11) + 1;
-        console.log(randomValue);
+        for (var i = 0; i < 4; i++) {
+            var randomValue = Math.floor(Math.random() * 11) + 1;
+            console.log(randomValue);
 
-        var imageCrystal = $("<img src = '" + pics[i] + "'>");
+            var imageCrystal = $("<img src = '" + pics[i] + "'>");
 
-        imageCrystal.addClass("crystal-image");
+            imageCrystal.addClass("crystal-image");
 
-        // imageCrystal.attr("src", pics[i]);
+            // imageCrystal.attr("src", pics[i]);
 
-        imageCrystal.attr("data-random-value", randomValue);
+            imageCrystal.attr("data-random-value", randomValue);
 
-        $(".crystals").append(imageCrystal);
+            $(".crystals").append(imageCrystal);
+
+        };
 
     };
+    begin();
 
-};
-begin();
+    $(document).on("click", ".crystal-image", function () {
 
-$(document).on("click", ".crystal-image", function () {
+        var num = parseInt($(this).attr("data-random-value"));
 
-    var num = parseInt($(this).attr("data-random-value"));
+        totalScore += num;
 
-    totalScore += num;
+        $("#totalScore").text(totalScore);
+        console.log(totalScore);
 
-    $("#totalScore").text(totalScore);
-    console.log(totalScore);
+        if (totalScore > randomNumber) {
+            losses++;
+            $("#losses").text(losses);
+            begin();
+            $("#totalScore").empty();
+            totalScore = 0;
+            alert("You Lose");
+            console.log("you lose")
+        }
 
-    if (totalScore > randomNumber) {
-        losses++;
-        $("#losses").text(losses);
-        begin();
-        totalScore = 0;
-        alert("You Lose");
-        console.log("you lose")
-    }
+        else if (totalScore === randomNumber) {
+            wins++;
+            $("#wins").text(wins);
+            begin();
+            $("#totalScore").empty();
+            totalScore = 0;
+            alert("You Win");
+        }
 
-    else if (totalScore === randomNumber) {
-        wins++;
-        $("#wins").text(wins);
-        begin();
-        totalScore = 0;
-        alert("You Win");
-    }
-
-});
+    });
 
 });
